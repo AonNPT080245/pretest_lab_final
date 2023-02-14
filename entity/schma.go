621bd757497923ca64f8db758545c00863d67dfc
AonNPT080245 <nptkkp.1048@gmail.com>
2023-02-14 23:37:32 +0700

    create struct user - close #4

--- entity/schma.go
+++ entity/schma.go
@@ -9,3 +9,12 @@ type Video struct {
 	Name string `valid:"required~Name cannot be blank"`
 	Url  string `gorm:"uniqueIndex" valid:"url"`
 }
+
+type User struct {
+	gorm.Model
+	Name        string `valid:"required~Name cannot be blank"`
+	Email       string `valid:"email~Email invalid format"`
+	StudentID   string `gorm:"uniqurIndex" valid:"mathces(^[B]\\d{7,7}$)"`
+	PhoneNumber string `valid:"mathces(^([+]66|0)+[0-9]{9,9}$)"`
+	Password    string `valid:"matches(^minstringlength(8)$)~Password is not less than 8 characters ,maxstringlength(20)~Password is not more than 20 characters"`
+}
